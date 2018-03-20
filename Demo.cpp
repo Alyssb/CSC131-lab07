8c4bbed56edc29341fd5fd221a3f8ff0f566707c
Mr. Daehn <jdaehn@missouristate.edu>
2018-03-20 12:17:05 -0500

    LAB07-DEVELOP - Update demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  int num1{5};
+  int num2{4};
+  int sum = num1 + num2;
+  std::cout << "sum = " << sum << std::endl;
+  return EXIT_SUCCESS;
+}
